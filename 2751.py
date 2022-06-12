e234cf2837162b6a1f29d8d13cad55520dabc0ee
smjsih@naver.com <smjsih@naver.com>
2022-06-12 23:31:04 +0900

    알고리즘 공부(다익스트라 , 위상정렬, 최소 신장트리Kruskal)

--- 2751.py
+++ 2751.py
@@ -4,7 +4,7 @@ import sys
 n = int(input())
 data = []
 for i in range(n):
-    data.append(int(sys.stdin.readline().rstrip()))
+    data.append(sys.stdin.readline().rstrip())
 
 for i in sorted(data):
     sys.stdout.write(str(i)+'\n')
